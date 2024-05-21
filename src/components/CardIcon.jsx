d3cad7b953a88fb1f8f7cbd97371c1fba18513ed
yanabilous <yanabilous05@gmail.com>
2024-05-21 19:43:43 +0300

    added svg

--- src/components/CardIcon.jsx
+++ src/components/CardIcon.jsx
@@ -4,9 +4,8 @@ function CardIcon({iconName, className, color}) {
 
   return (
     <div className="header">
-      <svg className={`${className} img`} style={{fill: color}}>
+      <svg className={`${className} img`} style={{fill: color}} width={100} height={100}>
         <use xlinkHref={Icons +`${iconName}`}></use>
-        {/*<use href={`${Icons}#${iconName}`} />*/}
       </svg>
     </div>
   );
--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -4,15 +4,16 @@ import {cardsData} from "../utils/data";
 import Card from "../components/Card.jsx";
 
 
+
 function CardList() {
-  const content = cardsData.map(item => (
+  const cards = cardsData.map(item => (
     <Card key={item.id} item={item}/>
   ));
 
 
   return (
     <ul className="list">
-      {content}
+      {cards}
     </ul>
   );
 }
