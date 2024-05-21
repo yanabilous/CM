75c22aa8d44c279d0a39c52e7bdc285c6e299526
yanabilous <yanabilous05@gmail.com>
2024-05-21 17:41:50 +0300

    added svg

--- src/components/CardIcon.jsx
+++ src/components/CardIcon.jsx
@@ -1,9 +1,11 @@
 import Icons from "../assets/sprite.svg";
 
-function CardIcon({iconName, className, color}) {
+// function CardIcon({iconName, className, color}) {
+function CardIcon({iconName, color}) {
   return (
     <div className="header">
-      <svg className={`${className} img`} style={{fill: color}}>
+      {/*<svg className={`${className} img`} style={{fill: color}}>*/}
+      <svg className={`img`} style={{fill: color}}>
         <use href={Icons + iconName}></use>
       </svg>
     </div>
